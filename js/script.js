67891656f82f84e3d4741fc0b85e87f4945b6247
Michal Dokupil <michal@dokupil.pl>
2017-12-31 16:23:19 +0100

    Add DOM project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var buttonsPage = document.getElementsByClassName("button");
+
+    for (var i = 0; i < buttonsPage.length; i++) {
+        var buttonInnerText = buttonsPage[i].innerText;
+        alert(buttonInnerText);
+    }
+
